e407354702b6eb02a0a726df00b3d2f6e8baf1b6
Raul Alvarez <raulito2394@gmail.com>
2016-06-14 14:43:43 -0400

    added for loop

--- script.js
+++ script.js
@@ -2,20 +2,28 @@ $(document).ready(function(){
   $("#search").on("submit",function(e){
     e.preventDefault();
     var formData = {
-      't'              : $('input[name=t]').val(),
+      's'              : $('input[name=t]').val(),
+      'plot': "full"
     };
     $.ajax({   
       url: "http://www.omdbapi.com/?",   
       data: formData,
       dataType: "json",   
-      success: function(response) {     
-        console.log(response);  
-        $("#movie-title").html("Title : "+response.Title);   
-        $("#poster").attr("src", response.Poster);
-        $("#genre").html("Genre : "+response.Genre);  
-        $("#year").html("Year : "+response.Year);
-        $("#actors").html("Actors : "+response.Actors);
-        $("#plot").html("Plot : " + response.Plot);
+      success: function(response, callback) {     
+        for(var i = 0; i < response.Search.length; i++)
+          console.log(response.Search[i]);  
+        $("#multimedia").append(function()
+         { 
+            for(var i = 0; i < response.Search.length; i++)
+            {
+              $("#movie-title").html("Title : "+response.Search[i].Title);   
+              $("#poster").attr("src", response.Search[i].Poster);
+              $("#genre").html("Genre : "+response.Genre);  
+              $("#year").html("Year : "+response.Search[i].Year);
+              $("#actors").html("Actors : "+response.Search[i].Actors);
+              $("#plot").html("Plot : " + response.Search[i].Plot);
+        }
+        })
       } 
     });
   });
