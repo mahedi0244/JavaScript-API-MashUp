1d7f9ddb03d8ab3c638fc1c6c22f8c5ce392eac0
Raul Alvarez <raulito2394@gmail.com>
2016-06-14 23:47:09 -0400

    fixed appending issue

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ $(document).ready(function()
   $("#search").on("submit",function(e)
   {
     e.preventDefault();
-    var formData = {
-      's'              : $('input[name=t]').val(),
-      'plot': "full"
+    var formData = 
+    {
+      's': $('input[name=t]').val(),
     };
     $.ajax({   
       url: "http://www.omdbapi.com/?",   
@@ -13,16 +13,20 @@ $(document).ready(function()
       dataType: "json",   
       success: function(response, callback) 
       {     
+        $("#starter").html("");
+        // $(".multimedia").empty();
+        // $(".movie-title").empty();
+        // $(".poster").empty();
+        // $(".year").empty();
+
+
         for(var i = 0; i < response.Search.length; i++)
         {
           console.log(response.Search[i]);  
           var t = $("<div class='multimedia'> " + 
                     "<h2 class='movie-title'>" + response.Search[i].Title + "</h2>" +
                     "<img class='poster' src=' " + response.Search[i].Poster + "'/>" + 
-                    "<h3 class='year'>" + response.Search[i].Year + "</h3>" + 
-                    /*"<h3 class='genre'></h3>" + 
-                    "<h3 class='actors'></h3>" + 
-                    "<h3 class='plot'></h3>" + */
+                    "<h3 class='year'>Year: " + response.Search[i].Year + "</h3>" + 
                   "</div");
         $("#starter").append(t);
       }
