7c7b974c937f8828b5b5396e59af9bb74aff8e64
Raul Alvarez <raulito2394@gmail.com>
2016-06-14 23:23:23 -0400

    appending movies properly

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-  $("#search").on("submit",function(e){
+$(document).ready(function()
+{
+  $("#search").on("submit",function(e)
+  {
     e.preventDefault();
     var formData = {
       's'              : $('input[name=t]').val(),
@@ -9,21 +11,30 @@ $(document).ready(function(){
       url: "http://www.omdbapi.com/?",   
       data: formData,
       dataType: "json",   
-      success: function(response, callback) {     
+      success: function(response, callback) 
+      {     
         for(var i = 0; i < response.Search.length; i++)
+        {
           console.log(response.Search[i]);  
-        $("#multimedia").append(function()
+          var t = $("<div class='multimedia'> " + 
+                    "<h2 class='movie-title'>" + response.Search[i].Title + "</h2>" +
+                    "<img class='poster' src=' " + response.Search[i].Poster + "'/>" + 
+                    "<h3 class='year'>" + response.Search[i].Year + "</h3>" + 
+                    /*"<h3 class='genre'></h3>" + 
+                    "<h3 class='actors'></h3>" + 
+                    "<h3 class='plot'></h3>" + */
+                  "</div");
+        $("#starter").append(t);
+      }
+        /*$("#multimedia").append(function()
          { 
-            for(var i = 0; i < response.Search.length; i++)
-            {
               $("#movie-title").html("Title : "+response.Search[i].Title);   
               $("#poster").attr("src", response.Search[i].Poster);
               $("#genre").html("Genre : "+response.Genre);  
               $("#year").html("Year : "+response.Search[i].Year);
               $("#actors").html("Actors : "+response.Search[i].Actors);
               $("#plot").html("Plot : " + response.Search[i].Plot);
-        }
-        })
+        })*/
       } 
     });
   });
